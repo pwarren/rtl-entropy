27013337c597014bd13a750641ed911220d6c19e
Paul Warren <pwarren@pwarren.id.au>
2015-12-30 08:47:10 +1100

    more cleanup

--- src/brf_input.c
+++ src/brf_input.c
@@ -66,9 +66,6 @@ pthread_t rx_task;
 struct bladerf_stream *rx_stream;
 
 /* flags */
-int opt = 0;
-int redirect_output = 0;
-int gflags_encryption = 0;
 int output_ready;
 
 /* daemon */
@@ -86,246 +83,19 @@ void **buffers;
 unsigned int bitcounter = 0;
 unsigned int buffercounter = 0;
 
-/* Other bits */
-AES_KEY wctx;
-EVP_CIPHER_CTX en;
 
-void usage(void) {
-  fprintf(stderr,
-	  "brf_entropy, a high quality entropy source using RTL2832 based DVB-T receivers\n\n"
-	  "Usage: brf_entropy [options]\n"
-	  "\t-a Set gain (default: 1000)\n"
-	  "\t-d Device index (default: 0)\n"
-	  "\t-e Encrypt output\n"
-	  "\t-f Set frequency to listen (default: 434MHz )\n"
-	  "\t-s Samplerate (default: 40 MHz)\n");
-  fprintf(stderr,
-	  "\t-o Output file (default: STDOUT, /var/run/rtl_entropy.fifo for daemon mode (-b))\n"
-#ifndef __APPLE__
-	  "\t-p PID file (default: /var/run/rtl_entropy.pid)\n"
-	  "\t-b Daemonize\n"
-	  "\t-u User to run as (default: rtl_entropy)\n"
-	  "\t-g Group to run as (default: rtl_entropy)\n"
-#endif
-	  );
-  exit(EXIT_SUCCESS);
-}
-
-
-void parse_args(int argc, char ** argv) {
-  char *arg_string= "a:d:ef:g:o:p:s:u:hb";
-    
-  opt = getopt(argc, argv, arg_string);
-  while (opt != -1) {
-    switch (opt) {
-    case 'a':
-      gain = (int)(atof(optarg) * 10);
-      break;
-
-    case 'b':
-      gflags_detach = 1;
-      break;
-      
-    case 'd':
-      /* dev_index = atoi(optarg); */
-      break;
-
-    case 'e':
-      gflags_encryption = 1;
-      break;
-      
-    case 'f':
-      frequency = (uint32_t)atofs(optarg);
-      break;
-      
-    case 'g':
-      gid = parse_group(optarg);
-      break;
-      
-    case 'h':
-      usage();
-      break;
-      
-    case 'o':
-      redirect_output = 1;
-      output = fopen(optarg,"w");
-      if (output == NULL) {
-	suicide("Couldn't open output file");
-      }
-      fclose(stdout);
-      break;
-      
-    case 'p':
-      pidfile_path = strdup(optarg);
-      break;
-      
-    case 's':
-      samp_rate = (uint32_t)atofs(optarg);
-      break;
-      
-    case 'u':
-      uid = parse_user(optarg, &gid);
-      break;
-      
-    default:
-      usage();
-      break;
-    }
-    opt = getopt(argc, argv, arg_string);
-  }
-}
-
-static void sighandler(int signum)
-{
-  do_exit = signum;
-}
-
-#ifndef __APPLE__
-static void drop_privs(int uid, int gid)
-{
-  cap_t caps;
-  prctl(PR_SET_KEEPCAPS, 1);
-  caps = cap_from_text("cap_sys_admin=ep");
-  if (!caps)
-    suicide("cap_from_text failed");
-  if (setgroups(0, NULL) == -1)
-    suicide("setgroups failed");
-  if (setegid(gid) == -1 || seteuid(uid) == -1)
-    suicide("dropping privs failed");
-  if (cap_set_proc(caps) == -1)
-    suicide("cap_set_proc failed");
-  cap_free(caps);
-}
-#endif
-
-void route_output(void) {
-  /* Redirect output if directed */   
-  if (!redirect_output) {
-    if (mkfifo(DEFAULT_OUT_FILE,S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) {
-      if (errno != EEXIST) {
-	perror("Bad FIFO");
-      }
-    }
-    log_line(LOG_INFO, "Waiting for a Reader...");
-    output = fopen(DEFAULT_OUT_FILE,"w");
-    if (output == NULL) {
-      suicide("Couldn't open output file");
-    }
-    redirect_output = 1;
-    fclose(stdout);
-  }
-}
-
+/* on Callback, write the output, dispatch a handler?
+ * Not sure on architecture here.
+ *
+ */
 static void *rx_stream_callback(struct bladerf *dev,
 				struct bladerf_stream *stream,
 				struct bladerf_metadata *meta,
 				void *samples,
 				size_t num_samples,
 				void *user_data)
-{
-  int i ,j;
-  int16_t *sample = (int16_t *)samples;
-  int ch, ch2;
-  int bufsize = num_samples/2;
-  uint8_t *ciphertext;
-  int fips_result;
-  int aes_len;
-  uint32_t out_block_size = MAXIMAL_BUF_LENGTH;
-  
-  for(i=0; i<num_samples * 2; i++) {
-    for (j=0; j < 10; j+= 2) {
-      ch = (*sample >> j) & 0x01;
-      ch2 = (*sample >> (j+1)) & 0x01;
-      if (ch != ch2) {
-	if (ch) {
-	  /* store a 1 in our bitbuffer */
-	  bitbuffer[buffercounter] |= 1 << bitcounter;
-	} /* else, leave the buffer alone, it's already 0 at this bit */
-	bitcounter++;
-      } else {
-	if (ch) {
-	  store_hash_data(1);
-	} else {
-	  store_hash_data(0);
-	}
-      }
-      
-      /* is byte full? */
-      if (bitcounter >= sizeof(bitbuffer[0]) * 8) {
-	buffercounter++;
-	bitcounter = 0;
-      }
-      
-      /* is buffer full? */
-      if (buffercounter >= BUFFER_SIZE) {
-	/* We have 2500 bytes of entropy 
-	   Can now send it to FIPS! */
-	fips_result = fips_run_rng_test(&fipsctx, &bitbuffer);
-	if (!fips_result) {
-	  if (gflags_encryption != 0) {
-	    if (hash_loop) {
-	      /*   /\* Get a key from disacarded bits *\/ */
-	      SHA512(hash_data_buffer, sizeof(hash_data_buffer), hash_buffer);
-	      /* use key to encrypt output */
-	      /* AES_set_encrypt_key(hash_buffer, 128, &wctx); */
-	      /* AES_encrypt(bitbuffer, bitbuffer_old, &wctx); */
-	      aes_init(hash_buffer, sizeof(hash_buffer), &en);
-	      aes_len = sizeof(bitbuffer);
-	      ciphertext = aes_encrypt(&en, bitbuffer, &aes_len);
-	      /* yay, send it to the output! */
-	      fwrite(ciphertext,sizeof(ciphertext[0]),aes_len,output);
-	      /* Clean up */
-	      free(ciphertext);
-	      EVP_CIPHER_CTX_cleanup(&en);
-	    }
-	  } else {
-	    /* xor with old data */
-	    for (buffercounter = 0; buffercounter < BUFFER_SIZE; buffercounter++) {
-	      bitbuffer[buffercounter] = bitbuffer[buffercounter] ^ bitbuffer_old[buffercounter];
-	    }
-	    if (output_ready > 2) {
-	    fwrite(&bitbuffer_old,sizeof(bitbuffer_old[0]),BUFFER_SIZE,output);
-	    }
-	    /* swap old data */
-	    memcpy(bitbuffer_old,bitbuffer,BUFFER_SIZE);
-	  }
-	  output_ready++;
-	} else {   /* FIPS test failed */
-	  for (j=0; j< N_FIPS_TESTS; j++) {
-	    if (fips_result & fips_test_mask[j]) {
-	      if (!gflags_detach)
-		log_line(LOG_DEBUG, "Failed: %s", fips_test_names[j]);
-	    }
-	  }
-	}
-	/* reset buffers, and the counter */
-	/* memset(bitbuffer_old,0,sizeof(bitbuffer_old)); */
-	memset(bitbuffer,0,sizeof(bitbuffer));
-	buffercounter = 0;
-      }
-    }
-    sample ++;
-  }
-
-  
-
-  
-  if (!do_exit) {
-    return samples;
-  } else {
-    if ( (do_exit == SIGPIPE) && gflags_detach) {
-      log_line(LOG_DEBUG, "Reader went away, closing FIFO");
-      fclose(output);
-      log_line(LOG_DEBUG, "Waiting for a Reader...");
-      output = fopen(DEFAULT_OUT_FILE,"w");
-      if (output == NULL) {
-	return NULL;
-      }
-      return samples;
-    } else {
-      return NULL;
-    }
-  }
+{ 
+  fwrite(&samples,num_samples,BUFFER_SIZE,output);
 }
 
 void * rx_task_run(void *inputs) {
@@ -338,35 +108,12 @@ void * rx_task_run(void *inputs) {
 }
 
 
-
+/* TODO will have to rename main */
 int main(int argc, char **argv) {
   struct sigaction sigact;
   int samples_per_buffer = 65536;
   int r;
 
-  parse_args(argc, argv);
-  
-  if (gflags_detach) {
-#ifndef __APPLE__
-    daemonize();
-#endif
-  }
-  log_line(LOG_INFO,"Options parsed, continuing.");
-  
-#ifndef __APPLE__  
-  if (uid != -1 && gid != -1)
-    drop_privs(uid, gid);
-#endif
-
-  /* Setup Signal handlers */
-  sigact.sa_handler = sighandler;
-  sigemptyset(&sigact.sa_mask);
-  sigact.sa_flags = 0;
-  sigaction(SIGINT, &sigact, NULL);
-  sigaction(SIGTERM, &sigact, NULL);
-  sigaction(SIGQUIT, &sigact, NULL);
-  sigaction(SIGPIPE, &sigact, NULL);
-  
   /* Open device */
   r = bladerf_open(&dev, NULL);
   if (r < 0) {
@@ -442,15 +189,6 @@ int main(int argc, char **argv) {
 			  samples_per_buffer,
 			  32,
 			  NULL);
-  
-  
-  log_line(LOG_DEBUG, "Doing FIPS init");
-  fips_init(&fipsctx, (int)0);
-
-  if (gflags_detach)
-    route_output();
-  if (!redirect_output)
-    output = stdout;
     
   log_line(LOG_DEBUG, "Reading samples!");
   output_ready = 0;
