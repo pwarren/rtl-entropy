cbd8826447451a4dc792ffc27441f8313a8af958
Matthew Weger <matthew.d.weger@gmail.com>
2016-03-26 01:32:15 -0400

    debianization

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(rtl-entropy):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="rtl-entropy" command="/usr/bin/rtl-entropy"
---
+++ debian/rtl-entropy.default.ex
@@ -0,0 +1,10 @@
+# Defaults for rtl-entropy initscript
+# sourced by /etc/init.d/rtl-entropy
+# installed at /etc/default/rtl-entropy by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
